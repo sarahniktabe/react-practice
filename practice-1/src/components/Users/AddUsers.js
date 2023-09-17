3b875dbe7fe6eeb47c7de55322fed854b001e3c1
sara niktabe <sara.niktabe@gmail.com>
2023-09-17 11:22:43 +0330

    Custom Button

--- practice-1/src/components/Users/AddUsers.js
+++ practice-1/src/components/Users/AddUsers.js
@@ -1,15 +1,21 @@
+import Card from "../Ul/Card";
+import classes from "./AddUsers.module.css";
+import Button from "../Ul/Button";
+
 const AddUsers = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">User Name</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">age(years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">age(years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
