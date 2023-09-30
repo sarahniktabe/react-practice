673ef290680a4c5e97e6fda2b9e58c8cf23bc8b7
sara niktabe <sara.niktabe@gmail.com>
2023-09-30 10:39:26 +0330

    added array

--- practice-2/src/components/map-of-iran.jsx
+++ practice-2/src/components/map-of-iran.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React from "react";
 
 function MapOfIran() {
-  return (
- <>
- </>
-  )
+  const buyersList = [
+    {
+      id: 1,
+      image: "ahvaz.webp",
+      name: "روزبه",
+      city: "اهواز",
+      whatBuy: "ساعت",
+      top: "top-[180px]",
+      topLg: "lg:top-[360px]",
+      right: "right-[195px]",
+      rightLg: "lg:right-[390px]",
+    },
+    {
+      id: 2,
+      image: "zan2.webp",
+      name: "لیلی",
+      city: "چالدران",
+      whatBuy: "مانتو",
+      top: "top-[10px]",
+      topLg: "lg:top-[20px]",
+      left: "left-[5px]",
+      leftLg: "lg:left-[10px]",
+      translateX: "-translate-x-[45px]",
+      translateXLg: "lg:-translate-x-[90px]",
+    },
+    {
+      id: 3,
+      image: "zan1.webp",
+      name: "سارا",
+      city: "کاشمر",
+      whatBuy: "لوازم خانگی",
+      top: "top-[90px]",
+      topLg: "lg:top-[180px]",
+      right: "right-[15px]",
+      rightLg: "lg:right-[30px]",
+    },
+    {
+      id: 4,
+      image: "kerman.webp",
+      name: "علی",
+      city: "کرمان",
+      whatBuy: "کفش چرم",
+      top: "top-[205px]",
+      topLg: "lg:top-[410px]",
+      right: "right-[35px]",
+      rightLg: "lg:right-[70px]",
+    },
+    {
+      id: 5,
+      image: "tehran.webp",
+      name: "هادی",
+      city: "تهران",
+      whatBuy: "ادکلن",
+      top: "top-[85px]",
+      topLg: "lg:top-[170px]",
+      right: "right-[158px]",
+      rightLg: "lg:right-[316px]",
+    },
+  ];
+  return <></>;
 }
 
 export default MapOfIran;
